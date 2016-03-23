a6af69fec92a3dc750b5fc9b30a70999bb37f24e
jennifer-wung <jennifer790709@gmail.com>
2016-03-22 18:04:42 -0700

    changed package name

--- calculate-page-rank/run2.sh
+++ calculate-page-rank/run2.sh
@@ -6,7 +6,7 @@ javac -classpath ../../hadoop-core-1.2.1.jar -d classFolder/ *.java
 jar -cvf  pageRank.jar -C classFolder .
 
 # execute
-hadoop jar pageRank.jar part2.PageRankingJob /opt/input/1G/input-1G output1G
+hadoop jar pageRank.jar calculate-page-rank.PageRankingJob /opt/input/1G/input-1G output1G
 
 # see result
 hadoop fs -cat output1G/result/part-00000
