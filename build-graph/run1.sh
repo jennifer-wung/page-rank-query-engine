c20516a92eae104250fb357ac991fbc28f57f795
jennifer-wung <jennifer790709@gmail.com>
2016-03-22 17:58:34 -0700

    changed package name

--- build-graph/run1.sh
+++ build-graph/run1.sh
@@ -7,7 +7,7 @@ javac -classpath ../../hadoop-core-1.2.1.jar -d classFolder/ *.java
 jar -cvf  pageGraph.jar -C classFolder .
 
 # execute
-hadoop jar pageGraph.jar part1.GraphJob input1M output1M
+hadoop jar pageGraph.jar build-graph.GraphJob input1M output1M
 
 # see result
 hadoop fs -cat output1M/part-00000
